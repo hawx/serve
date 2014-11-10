7cda902e10cae35dffad60a04cf7e3c03f3e0902
Joshua Hawxwell <m@hawx.me>
2014-11-10 20:30:52 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# servers-gonna-serve
+
+A small package that wraps up serving a `http.Handler` via a port or socket. It
+catches interrupts so any `defer`s will run properly.
+
+``` go
+package main
+
+import (
+  "github.com/hawx/serve"
+  "net/http"
+  "flag"
+)
+
+var (
+  port   = flag.String("port", "8080", "")
+  socket = flag.String("socket", "", "")
+)
+
+func main() {
+  flag.Parse()
+
+  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+    // ...
+  })
+
+  serve.Serve(*port, *socket, http.DefaultServeMux)
+}
+```
---
+++ serve.go
@@ -0,0 +1,50 @@
+package serve
+
+import (
+	"net"
+	"net/http"
+	"log"
+	"os"
+	"os/signal"
+)
+
+func Serve(port, socket string, handler http.Handler) {
+	if socket == "" {
+		Port(port, handler)
+	} else {
+		Socket(socket, handler)
+	}
+}
+
+func Socket(socket string, handler http.Handler) {
+	l, err := net.Listen("unix", socket)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer l.Close()
+
+	go func() {
+		log.Println("listening on", socket)
+		log.Fatal(http.Serve(l, handler))
+	}()
+
+	catchInterrupt()
+}
+
+func Port(port string, handler http.Handler) {
+	go func() {
+		log.Println("listening on port :" + port)
+		log.Fatal(http.ListenAndServe(":"+port, handler))
+	}()
+
+	catchInterrupt()
+}
+
+func catchInterrupt() {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, os.Kill)
+
+	s := <-c
+	log.Printf("caught %s: shutting down", s)
+}
